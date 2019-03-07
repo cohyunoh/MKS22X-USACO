6903382179d50cc80a0007331f925601a40e260c
cohyunoh <coh00@stuy.edu>
2019-03-06 22:32:33 -0500

    stomp had the same problem

--- USACO.java
+++ USACO.java
@@ -54,8 +54,10 @@ public class USACO{
       greatest = findGreatest(row,col,pasture);
       for(int r = 0; r < 3; r++){
         for(int c = 0; c < 3; c++){
-          if(pasture[row + r][col + c] == greatest){
-            pasture[row + r][col + c]--;
+          if(row + r < pasture.length && col + c < pasture[row].length){
+            if(pasture[row + r][col + c] == greatest){
+              pasture[row + r][col + c]--;
+            }
           }
         }
       }
